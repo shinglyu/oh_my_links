1ec9f1f9d065174d50d240a4319304fed03c5950
Shing Lyu <shing.lyu@gmail.com>
2019-03-11 17:06:47 +0800

    Add works

--- src/browser_action/popup.js
+++ src/browser_action/popup.js
@@ -1,29 +1,48 @@
 async function getList() {
   try {
     const items = await chrome.storage.local.get('list');
-    return new Set(items['list']);
+    return JSON.parse(items['list']);
   }
   catch {
-    return new Set([]);
+    return [];
   }
 }
 
 async function addToList(url) {
   let list = await getList();
-  list.add(url);
-  
+  listSet = new Set(list);
+  listSet.add(url);
   await chrome.storage.local.set({
-    'list': JSON.stringify(list),
+    'list': JSON.stringify(Array.from(listSet)),
   });
+}
+
+async function renderList(containerId) {
+  const container = document.getElementById(containerId);
+
+  // Reset
+  while (container.firstChild) {
+    container.removeChild(container.firstChild);
+  } 
+
+  const list = await getList();
+  for (const url of list) {
+    var listItem = document.createElement('li');
+    listItem.textContent = url;
+    container.appendChild(listItem);
+  }
 }
 
 async function main() {
   document.addEventListener('DOMContentLoaded', function(){
+    renderList('linkList');
+
     const addButton = document.getElementById('addCurrentTab');
     addButton.addEventListener('click', async function(event){
       const tabs = await chrome.tabs.query({'active': true, 'currentWindow': true});
       const url = tabs[0].url;
-      alert(url);
+      addToList(url);
+      renderList('linkList');
     });
   })
 }
